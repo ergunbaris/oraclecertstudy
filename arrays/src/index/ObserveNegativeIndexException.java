b76154bd640f1a672809fb84d4add75d97764bb2
bergun <barisergun75@gmail.com>
2014-12-01 05:52:27 +0200

    assignments7

---
+++ arrays/src/index/ObserveNegativeIndexException.java
@@ -0,0 +1,9 @@
+package index;
+
+public class ObserveNegativeIndexException{
+    public static void main (String [] args){
+	int z = -3;
+	int [] intArray = {0,1};
+	System.out.println(intArray[z]);
+    }
+}
---
+++ strings/src/methods/StringTrim.java
@@ -0,0 +1,7 @@
+package methods;
+class StringTrim{
+    public static void main(String...args){
+	String s3 = " hi hi asdas      ";
+	System.out.println(s3.trim());
+    }
+}

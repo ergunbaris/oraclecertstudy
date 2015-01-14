1a75165e60a5282ca4521e204a9a9d79148f8b03
bergun <barisergun75@gmail.com>
2015-01-14 07:57:09 +0200

    assignment9

---
+++ assignments/src/primwrappers/IsShortEqual.java
@@ -0,0 +1,14 @@
+package primwrappers;
+class IsShortEqual{
+   public static void main(String...args){
+	Short sample1 = 123;
+	Short sample2 = 123;
+
+	Short sample3 = 129;
+	Short sample4 = 129;
+
+	System.out.println(sample1==sample2);
+	System.out.println(sample3==sample4);
+
+   }
+}

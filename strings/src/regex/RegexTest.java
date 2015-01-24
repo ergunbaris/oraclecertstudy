a9fc9d871dfc6b3c18cd59372a83155e6bab5411
bergun <barisergun75@gmail.com>
2015-01-24 11:27:03 +0200

    string2

---
+++ strings/src/regex/RegexTest.java
@@ -0,0 +1,23 @@
+package regex;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+public class RegexTest{
+    public static void main(String...args){
+	if(args.length != 2){
+	    throw new IllegalArgumentException("there must be two arguments");
+	}
+	String source = args[1];
+	String expression = args[0];
+	Pattern p = Pattern.compile(args[0]);
+	Matcher m = p.matcher(args[1]);
+	System.out.println("\nsource: " + source);
+	System.out.println(" index: 01234567890123456");
+	System.out.println("\nexpression: " + m.pattern());
+	System.out.print("match positions: ");
+	while(m.find()){	    
+  	    System.out.print(m.start() + " ");
+	}
+	System.out.println();
+	
+    }
+}

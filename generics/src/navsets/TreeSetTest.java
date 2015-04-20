d1306c3c9580e3d158f2c04205cc8b4c0aa4db45
bergun <barisergun75@gmail.com>
2015-04-20 13:34:56 +0300

    gen2 testing subSet, tailSet and headSet methods

--- generics/src/navsets/TreeSetTest.java
+++ generics/src/navsets/TreeSetTest.java
@@ -11,31 +11,43 @@ class TreeSetTest{
         sortedSet.add(77);
         sortedSet.add(51);
         sortedSet.add(40);
-        for(Integer found:sortedSet){
-            System.out.println(found);
-        }
+        System.out.println("sortedSet:" +sortedSet);
+
         NavigableSet<Integer> subset = new TreeSet<>();
         subset = sortedSet.subSet(51,true,89,true); // after Java 6
-        System.out.println(subset);
+        System.out.println("subset:" + subset);
         
         SortedSet<Integer> subset2 = new TreeSet<>();
         subset2 = sortedSet.subSet(51,89); // existed befor Java 6
-        System.out.println(subset2);
+        System.out.println("subset2:" + subset2);
 
         NavigableSet<Integer> subset3 = new TreeSet<>();
         subset3 = sortedSet.headSet(51,true); // after Java 6
-        System.out.println(subset3);
+        System.out.println("subset3:" + subset3);
                 
         SortedSet<Integer> subset4 = new TreeSet<>();
         subset4 = sortedSet.headSet(51); // existed befor Java 6
-        System.out.println(subset4);
+        System.out.println("subset4:" + subset4);
 
         NavigableSet<Integer> subset5 = new TreeSet<>();
         subset5 = sortedSet.tailSet(89,false); // after Java 6
-        System.out.println(subset5);
+        System.out.println("subset5:" + subset5);
                 
         SortedSet<Integer> subset6 = new TreeSet<>();
         subset6 = sortedSet.tailSet(89); // existed befor Java 6
-        System.out.println(subset6);
+        System.out.println("subset6:" + subset6);
+
+        Integer polledInt = subset.pollFirst();
+        System.out.printf("polled Integer:%d\n",polledInt);
+        // would be polled from both collections sortedset and subset(which is the backed collection)
+        System.out.println("subset:" + subset);
+        System.out.println("sortedSet:" + sortedSet);
+        
+        Integer polledInt2 = sortedSet.pollFirst();
+        System.out.printf("polled Integer:%d\n",polledInt2);
+        // would be polled from only from sortedset (because it doesnt exist in the backed collection)
+        System.out.println("subset:" + subset);
+        System.out.println("sortedSet:" + sortedSet);
+        
     }
 }

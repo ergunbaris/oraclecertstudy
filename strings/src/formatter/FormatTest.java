4958a9f379824f82400e077f37a510c748d40f34
bergun <barisergun75@gmail.com>
2015-06-07 15:03:45 +0300

    string4 - left allign must be used with format width spacing

--- strings/src/formatter/FormatTest.java
+++ strings/src/formatter/FormatTest.java
@@ -4,5 +4,6 @@ public class FormatTest{
     public static void main(String...args){
 	System.out.format(">%2$0(7d< + >%1$+-9.2f< \n",-500.235F,100);
 	System.out.printf(">%(-9.4f< \n",-1.23456F);
+	System.out.printf(">%(-.4f< \n",-1.23456F); // Missing format width expression
     }
 }
